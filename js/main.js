9516d01cec3436b049b7f1801644dc9956b8623b
kusd <dx0181@163.com>
2019-06-09 01:49:40 +0800

    doc:文档记录

--- js/main.js
+++ js/main.js
@@ -1,69 +1,80 @@
-$(window).load(function(){
-     $('.preloader').fadeOut('slow');
+
+$(document).ready(function(e) {
+	$("html,#about_content").niceScroll({ autohidemode: false });
+	$("a.signUp").click(function(e){
+		e.preventDefault();
+		//alert();
+		$(this).parent("p").parent("div.login").hide(500, false, function(){
+			$(this).next("div.signup").show(500);
+		});
+		
+	});
+	$("input.logIn").click(function(e){
+		e.preventDefault();
+		//alert();
+		$(this).parent("form").parent("div.signup").hide(500, false, function(){
+			$(this).prev("div.login").show(500);
+		});
+		
+	});
+
+	$('.menu > li').click(function(){
+		$('.menu > li').not(this).removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$("ul.menu").children("li").children("a").click(function(){
+		$(this).next("ul").stop().slideToggle(300);
+	});
+	$("a#addCntnt").click(function(){
+		$(".content").stop().fadeOut(300);
+		$("#aContent").stop().slideDown(300);
+	});
+	$("a#vCont").click(function(){
+		$(".content").stop().fadeOut(300);
+		$("#viewContent").stop().slideDown(300);
+	});
+	$("a#addMenu").click(function(){
+		$(".content").stop().fadeOut(300);
+		$("#aMenu").stop().slideDown(300);
+	});
+	$("a#vMenu").click(function(){
+		$(".content").stop().fadeOut(300);
+		$("#viewMenus").stop().slideDown(300);
+	});
+	$("span.fa-close").click(function(){
+		$(this).parent("h4").parent("div").stop().slideUp(300);
+	});
+	$('#button-send').click(function(event){
+		$('#button-send').html('Sending E-Mail...');
+		event.preventDefault();		
+		$.ajax({
+			type: 'POST',
+			url: 'send_form_email.php',
+			data: $('#contact_form').serialize(),
+			success: function(html) {
+				if(html.success == '1')
+				{
+					$('#button-send').html('Send');
+					$('#success').show();
+				}
+				else
+				{
+					$('#button-send').html('Send');
+					$('#error').show();
+				}					
+			},
+			error: function(){
+				$('#button-send').html('Send');
+				$('#error').show();
+			}
+		});
+		
+	});
 });
 
 
-/* =Main INIT Function
--------------------------------------------------------------- */
-function initializeSite() {
-
-	"use strict";
-
-	//OUTLINE DIMENSION AND CENTER
-	(function() {
-	    function centerInit(){
-
-			var sphereContent = $('.sphere'),
-				sphereHeight = sphereContent.height(),
-				parentHeight = $(window).height(),
-				topMargin = (parentHeight - sphereHeight) / 2;
-
-			sphereContent.css({
-				"margin-top" : topMargin+"px"
-			});
-
-			var heroContent = $('.hero'),
-				heroHeight = heroContent.height(),
-				heroTopMargin = (parentHeight - heroHeight) / 2;
-
-			heroContent.css({
-				"margin-top" : heroTopMargin+"px"
-			});
-
-	    }
-
-	    $(document).ready(centerInit);
-		$(window).resize(centerInit);
-	})();
-
-	// Init effect
-	$('#scene').parallax();
-
-};
-/* END ------------------------------------------------------- */
-
-/* =Document Ready Trigger
--------------------------------------------------------------- */
-$(window).load(function(){
-
-	initializeSite();
-	(function() {
-		setTimeout(function(){window.scrollTo(0,0);},0);
-	})();
-
-});
-/* END ------------------------------------------------------- */
-
-
-$('#countdown').countdown({
-	date: "December 13, 2025 18:03:26",
-	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>天</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>小时</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>分钟</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>秒</span></div>");
-	}
-});
+function valemail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
